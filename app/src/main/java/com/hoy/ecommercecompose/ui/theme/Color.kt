e5bda1329bb005af8021c5f0623dcd753867e6d2
altunoymak <yusuf_altunoymak@hotmail.com>
2024-07-30 12:52:07 +0300

    Configuration Theme

--- app/src/main/java/com/hoy/ecommercecompose/ui/theme/Color.kt
+++ app/src/main/java/com/hoy/ecommercecompose/ui/theme/Color.kt
@@ -1,11 +1,63 @@
 package com.hoy.ecommercecompose.ui.theme
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryColor = Color(0xFFEF8354)
+val SecondaryColor = Color(0xFF2D3142)
+val TertiaryColor = Color(0xFFEFB8C8)
+val PrimaryDarkColor = Color(0xFFD76D77)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+fun lightColors(
+    primary: Color = Color(0xFFFF5722),
+    primaryContainer: Color = Color(0xFF2D3142),
+    secondary: Color = Color(0xFF2196F3),
+    secondaryContainer : Color = Color(0xFF009688)
+): SampleProjectColor = SampleProjectColor(
+    primary = primary,
+    primaryContainer = primaryContainer,
+    secondary = secondary,
+    secondaryContainer = secondaryContainer
+)
+
+fun darkColors(
+    primary: Color = Color(0xFFFF5722),
+    primaryContainer: Color = Color(0xFF2D3142),
+    secondary: Color = Color(0xFF2196F3),
+    secondaryContainer : Color = Color(0xFF009688)
+): SampleProjectColor = SampleProjectColor(
+    primary = primary,
+    primaryContainer = primaryContainer,
+    secondary = secondary,
+    secondaryContainer = secondaryContainer
+)
+
+
+class SampleProjectColor(
+    primary: Color,
+    primaryContainer: Color,
+    secondary: Color,
+    secondaryContainer: Color
+) {
+    private var _primary: Color by mutableStateOf(primary)
+    val primary: Color = _primary
+
+    private var _primaryContainer: Color by mutableStateOf(primaryContainer)
+    val primaryContainer: Color = _primaryContainer
+
+    private var _secondary: Color by mutableStateOf(secondary)
+    val secondary: Color = _secondary
+
+    private var _secondaryContainer: Color by mutableStateOf(secondaryContainer)
+    val secondaryContainer: Color = _secondaryContainer
+
+    private class KelpColorPreview {
+        val primary_FF000000_FFFFFFFF = Unit
+        val primaryContainer_FFFFFFFF_FF000000 = Unit
+    }
+
+}
+internal val LocalColors = staticCompositionLocalOf { lightColors() }
--- app/src/main/java/com/hoy/ecommercecompose/ui/theme/Type.kt
+++ app/src/main/java/com/hoy/ecommercecompose/ui/theme/Type.kt
@@ -4,7 +4,31 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.hoy.ecommercecompose.R
+
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val bodyFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Barlow"),
+        fontProvider = provider,
+    )
+)
+
+val displayFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Poppins"),
+        fontProvider = provider,
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
