51ee2ae31f4d97b81b1b8645a01ab52f71ca1cfc
altunoymak <yusuf_altunoymak@hotmail.com>
2024-07-31 13:25:26 +0300

    SignUpContract and update SignUpViewModel

--- app/src/main/java/com/hoy/ecommercecompose/MainActivity.kt
+++ app/src/main/java/com/hoy/ecommercecompose/MainActivity.kt
@@ -4,18 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.hoy.ecommercecompose.ui.navigation.SetupNavGraph
-import com.hoy.ecommercecompose.ui.signup.SignupRoute
 import com.hoy.ecommercecompose.ui.theme.ECommerceComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
---
+++ app/src/main/java/com/hoy/ecommercecompose/ui/signup/SignUpContract.kt
@@ -0,0 +1,20 @@
+package com.hoy.ecommercecompose.ui.signup
+
+object SignUpContract {
+    data class UiState(
+        val isLoading: Boolean = false,
+        val email: String = "",
+        val password: String = "",
+        val name: String = "",
+        val surname: String = "",
+        val address: String = "",
+    )
+
+    sealed class UiAction {
+        data object SignUpClick : UiAction()
+        data class ChangeEmail(val email: String) : UiAction()
+        data class ChangePassword(val password: String) : UiAction()
+        data class ChangeName(val name: String) : UiAction()
+        data class ChangeSurname(val surname: String) : UiAction()
+    }
+}
