f3d12f2bd449f1ab9c958ae5ced90fefaa706bc1
altunoymak <yusuf_altunoymak@hotmail.com>
2024-08-08 12:08:45 +0300

    favorite handle in home and favorite screens

--- app/src/main/java/com/hoy/ecommercecompose/ui/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/hoy/ecommercecompose/ui/favorite/FavoriteScreen.kt
@@ -4,26 +4,19 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
@@ -43,18 +36,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.google.firebase.auth.FirebaseAuth
 import com.hoy.ecommercecompose.R
 import com.hoy.ecommercecompose.domain.model.ProductUi
 import com.hoy.ecommercecompose.ui.theme.LocalColors
@@ -68,11 +62,9 @@ fun FavoriteScreen(
 ) {
     val uiState by viewModel.uiState.collectAsState()
 
-    val currentUser = FirebaseAuth.getInstance().currentUser
-    LaunchedEffect(currentUser) {
-        currentUser?.uid?.let { userId ->
-            viewModel.loadFavorites()
-        }
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.loadFavorites()
     }
 
     Scaffold(
@@ -81,7 +73,7 @@ fun FavoriteScreen(
                 title = { Text("Favorite Products") },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
                     }
                 }
             )
@@ -92,6 +84,7 @@ fun FavoriteScreen(
                 uiState.isLoading -> {
                     CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
                 }
+
                 uiState.errorMessage != null -> {
                     Text(
                         text = uiState.errorMessage!!,
@@ -99,18 +92,20 @@ fun FavoriteScreen(
                         modifier = Modifier.align(Alignment.Center)
                     )
                 }
+
                 uiState.favoriteProducts.isEmpty() -> {
                     Text(
                         text = "No favorite products found.",
                         modifier = Modifier.align(Alignment.Center)
                     )
                 }
+
                 else -> {
-                    LazyVerticalGrid(
-                        columns = GridCells.Fixed(2)
-                    ) {
+                    LazyColumn {
                         items(uiState.favoriteProducts) { product ->
-                            ProductCard(product = product, onFavoriteClick = { /* Handle click */ })
+                            FavoriteProductCard(product = product, onFavoriteClick = {
+                                viewModel.deleteFavorite(it)
+                            })
                         }
                     }
                 }
@@ -120,99 +115,109 @@ fun FavoriteScreen(
 }
 
 @Composable
-fun ProductCard(
+fun FavoriteProductCard(
     product: ProductUi,
-    onFavoriteClick: () -> Unit,
+    onFavoriteClick: (ProductUi) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Card(
         modifier = modifier
-            .size(170.dp, 270.dp)
+            .fillMaxWidth()
             .padding(8.dp)
-            .clip(RoundedCornerShape(8.dp)),
+            .clip(RoundedCornerShape(12.dp))
+            .shadow(4.dp, RoundedCornerShape(12.dp)),
         border = BorderStroke(1.dp, Color.Gray),
         colors = CardDefaults.cardColors(
             containerColor = Color.White
         )
     ) {
-        Column(
+        Row(
             modifier = Modifier
-                .padding(8.dp)
-                .fillMaxSize()
+                .padding(12.dp)
+                .fillMaxWidth()
         ) {
-            Box(
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(product.imageOne)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "Product Image",
+                error = painterResource(id = R.drawable.ic_broken_image),
+                placeholder = painterResource(id = R.drawable.loading_img),
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(150.dp)
-                    .padding(top = 8.dp)
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(vertical = 8.dp)
             ) {
-                AsyncImage(
-                    model = ImageRequest.Builder(context = LocalContext.current)
-                        .data(product.imageOne)
-                        .crossfade(true)
-                        .build(),
-                    contentDescription = "Product Image",
-                    error = painterResource(id = R.drawable.ic_broken_image),
-                    placeholder = painterResource(id = R.drawable.loading_img),
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(150.dp)
+                Text(
+                    text = product.title,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.DarkGray,
+                    fontFamily = displayFontFamily,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
 
-                IconButton(
-                    onClick = onFavoriteClick,
-                    modifier = Modifier
-                        .align(Alignment.TopEnd)
-                        .background(color = LocalColors.current.primary, shape = CircleShape)
-                        .padding(4.dp)
-                        .size(36.dp)
+                Spacer(modifier = Modifier.height(6.dp))
+
+                Row {
+                    Text(
+                        text = "$${product.price}",
+                        fontFamily = displayFontFamily,
+                        style = MaterialTheme.typography.bodyLarge.copy(textDecoration = TextDecoration.LineThrough),
+                        color = Color.Gray
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        text = "$${product.salePrice}",
+                        fontFamily = displayFontFamily,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.Red,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(6.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
                     Icon(
-                        imageVector = Icons.Default.Favorite,
-                        contentDescription = "Favorite",
-                        tint = Color.White,
-                        modifier = modifier.size(16.dp)
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "Rating",
+                        tint = LocalColors.current.primary,
+                        modifier = Modifier.size(16.dp)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = product.rate.toString(),
+                        fontFamily = displayFontFamily,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Gray
                     )
                 }
             }
 
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Text(
-                text = product.title,
-                style = MaterialTheme.typography.bodyLarge,
-                color = Color.DarkGray,
-                fontFamily = displayFontFamily,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Text(
-                text = "$${product.price}",
-                fontFamily = displayFontFamily,
-                style = MaterialTheme.typography.bodyLarge,
-                color = Color.Black,
-                fontWeight = FontWeight.Bold
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically
+            IconButton(
+                onClick = { onFavoriteClick(product) },
+                modifier = Modifier
+                    .background(color = Color.White, shape = CircleShape)
+                    .padding(6.dp)
+                    .size(40.dp)
             ) {
                 Icon(
-                    imageVector = Icons.Default.Star,
-                    contentDescription = "Rating",
-                    tint = LocalColors.current.primary,
-                    modifier = Modifier.size(16.dp)
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                Text(
-                    text = product.rate.toString(),
-                    fontFamily = displayFontFamily,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = Color.Gray
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Favorite",
+                    tint = Color.Red,
+                    modifier = Modifier.size(20.dp)
                 )
             }
         }
