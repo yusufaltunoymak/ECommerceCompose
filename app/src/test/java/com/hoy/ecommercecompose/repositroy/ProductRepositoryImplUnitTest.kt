477f3d989f5815f359ee3c71bb519749d9c11e08
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-09-04 22:23:10 +0300

    Written unit test for all functions in ProductRepositoryImpl

--- app/src/test/java/com/hoy/ecommercecompose/repositroy/ProductRepositoryImplUnitTest.kt
+++ app/src/test/java/com/hoy/ecommercecompose/repositroy/ProductRepositoryImplUnitTest.kt
@@ -1,7 +1,10 @@
-package com.hoy.ecommercecompose
+package com.hoy.ecommercecompose.repositroy
 
+import com.hoy.ecommercecompose.common.Resource
 import com.hoy.ecommercecompose.data.repository.ProductRepositoryImpl
 import com.hoy.ecommercecompose.data.source.local.ProductDao
+import com.hoy.ecommercecompose.data.source.local.payment.model.PaymentEntity
+import com.hoy.ecommercecompose.data.source.local.payment.model.ProductEntity
 import com.hoy.ecommercecompose.data.source.remote.ApiService
 import com.hoy.ecommercecompose.data.source.remote.model.Category
 import com.hoy.ecommercecompose.data.source.remote.model.CheckFavoriteResponse
@@ -13,7 +16,11 @@ import com.hoy.ecommercecompose.data.source.remote.model.response.ProductListDto
 import com.hoy.ecommercecompose.domain.model.BaseBody
 import com.hoy.ecommercecompose.domain.model.DeleteFromFavoriteBody
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -46,9 +53,9 @@ class ProductRepositoryImplUnitTest {
                     count = 50,
                     description = "A high-quality smartphone with a sleek design.",
                     id = 1,
-                    imageOne = "https://example.com/images/product1_img1.jpg",
-                    imageTwo = "https://example.com/images/product1_img2.jpg",
-                    imageThree = "https://example.com/images/product1_img3.jpg",
+                    imageOne = "",
+                    imageTwo = "",
+                    imageThree = "",
                     price = 999.99,
                     rate = 4.5,
                     salePrice = 899.99,
@@ -59,9 +66,9 @@ class ProductRepositoryImplUnitTest {
                     count = 20,
                     description = "A powerful and energy-efficient vacuum cleaner.",
                     id = 2,
-                    imageOne = "https://example.com/images/product2_img1.jpg",
-                    imageTwo = "https://example.com/images/product2_img2.jpg",
-                    imageThree = "https://example.com/images/product2_img3.jpg",
+                    imageOne = "",
+                    imageTwo = "",
+                    imageThree = "",
                     price = 299.99,
                     rate = 4.7,
                     salePrice = 279.99,
@@ -72,9 +79,9 @@ class ProductRepositoryImplUnitTest {
                     count = 100,
                     description = "A thrilling mystery novel by a best-selling author.",
                     id = 3,
-                    imageOne = "https://example.com/images/product3_img1.jpg",
-                    imageTwo = "https://example.com/images/product3_img2.jpg",
-                    imageThree = "https://example.com/images/product3_img3.jpg",
+                    imageOne = "",
+                    imageTwo = "",
+                    imageThree = "",
                     price = 19.99,
                     rate = 4.9,
                     salePrice = null,
@@ -97,10 +104,10 @@ class ProductRepositoryImplUnitTest {
         val expectedCategories = GetCategoriesResponse(
             categories = listOf(
                 Category(
-                    name = "Electronics", image = "https://example.com/images/product1_img1.jpg"
+                    name = "Electronics", image = ""
                 ), Category(
-                    name = "Home Appliances", image = "https://example.com/images/product2_img1.jpg"
-                ), Category(name = "Books", image = "https://example.com/images/product3_img1.jpg")
+                    name = "Home Appliances", image = ""
+                ), Category(name = "Books", image = "")
             )
         )
         `when`(mockApiService.getCategories()).thenReturn(expectedCategories)
@@ -117,9 +124,9 @@ class ProductRepositoryImplUnitTest {
                 count = 12,
                 description = "A high-quality smartphone with a sleek design.",
                 id = 1,
-                imageOne = "https://example.com/images/product1_img1.jpg",
-                imageTwo = "https://example.com/images/product1_img2.jpg",
-                imageThree = "https://example.com/images/product1_img3.jpg",
+                imageOne = "",
+                imageTwo = "",
+                imageThree = "",
                 price = 999.99,
                 rate = 4.5,
                 salePrice = 899.99,
@@ -177,9 +184,9 @@ class ProductRepositoryImplUnitTest {
                     count = 40,
                     description = "A high-quality smartphone with a sleek design.",
                     id = 1,
-                    imageOne = "https://example.com/images/product1_img1.jpg",
-                    imageTwo = "https://example.com/images/product",
-                    imageThree = "https://example.com/images/product",
+                    imageOne = "",
+                    imageTwo = "",
+                    imageThree = "",
                     price = 999.99,
                     rate = 4.5,
                     salePrice = 899.99,
@@ -264,4 +271,192 @@ class ProductRepositoryImplUnitTest {
         assertEquals(expectedByCategory, result)
         verify(mockApiService).getProductsByCategory(category = "Electronics")
     }
+
+    @Test
+    fun `test getCartProductsLocal returns expected data`() = runBlocking<Unit> {
+        val userId = "123"
+        val expectedCartProducts = listOf(
+            ProductEntity(
+                productId = 1,
+                id = userId,
+                category = "Electronics",
+                count = 12,
+                description = "A high-quality smartphone with a sleek design.",
+                imageOne = "",
+                imageThree = "",
+                imageTwo = "",
+                price = 999.99,
+                rate = 4.5,
+                salePrice = 899.99,
+                saleState = true,
+                title = "Smartphone Model X",
+                isFavorite = false,
+                quantity = 1
+            )
+        )
+        `when`(mockProductDao.getCartProducts(userId = userId)).thenReturn(
+            flowOf(
+                expectedCartProducts
+            )
+        )
+        val resultFlow = productRepositoryImpl.getCartProductsLocal(userId = userId)
+        resultFlow.collect() { result ->
+            assertTrue(result is Resource.Success)
+            assertEquals(expectedCartProducts, (result as Resource.Success).data)
+        }
+        verify(mockProductDao).getCartProducts(userId = userId)
+    }
+
+    @Test
+    fun `test getCartProductsLocal handles exception`() = runTest {
+        val userId = "123"
+        val errorMessage = "An error occurred"
+        `when`(mockProductDao.getCartProducts(userId)).thenReturn(flow {
+            throw Exception(errorMessage)
+        })
+        val resultFlow = productRepositoryImpl.getCartProductsLocal(userId)
+        resultFlow.collect() { result ->
+            assertTrue(result is Resource.Error)
+            assertEquals(errorMessage, (result as Resource.Error).message)
+        }
+        verify(mockProductDao).getCartProducts(userId)
+    }
+
+    @Test
+    fun `test addToCartProduct`() = runBlocking {
+        val product = ProductEntity(
+            productId = 1,
+            id = "123",
+            category = "Electronics",
+            count = 15,
+            description = "A high-quality smartphone with a sleek design.",
+            imageOne = "",
+            imageThree = "",
+            imageTwo = "",
+            price = 999.99,
+            rate = 4.5,
+            salePrice = 899.99,
+            saleState = true,
+            title = "Smartphone Model X",
+            isFavorite = false,
+            quantity = 1
+        )
+        productRepositoryImpl.addToCartProduct(product)
+        verify(mockProductDao).addToCartProduct(product)
+    }
+
+    @Test
+    fun `test deleteCartProduct`() = runBlocking {
+        val productId = 1
+        productRepositoryImpl.deleteFromCartProduct(productId)
+        verify(mockProductDao).deleteFromCartProduct(productId)
+    }
+
+    @Test
+    fun `test isProductInCart`() = runBlocking {
+        val productId = 1
+        `when`(mockProductDao.isProductInCart(productId)).thenReturn(true)
+        val result = productRepositoryImpl.isProductInCart(productId)
+        assertTrue(result)
+    }
+
+    @Test
+    fun `test clearCart`() = runBlocking {
+        val userId = "123"
+        productRepositoryImpl.clearCart(userId)
+        verify(mockProductDao).clearCart(userId)
+    }
+
+    @Test
+    fun `test updateCartProduct`() = runBlocking {
+        val product = ProductEntity(
+            productId = 1,
+            id = "123",
+            category = "Electronics",
+            count = 36,
+            description = "A high-quality smartphone with a sleek design.",
+            imageOne = "",
+            imageTwo = "",
+            imageThree = "",
+            price = 999.99,
+            rate = 4.5,
+            salePrice = 899.99,
+            saleState = true,
+            title = "Smartphone Model X",
+            isFavorite = false,
+            quantity = 1
+        )
+        productRepositoryImpl.updateCartProduct(product)
+        verify(mockProductDao).updateCartProduct(product)
+    }
+
+    @Test
+    fun `test addPaymentDetails`() = runBlocking {
+        val payment = PaymentEntity(
+            paymentId = 12,
+            userId = "123",
+            cardNumber = "4111111111111111",
+            cardHolderName = "John Doe",
+            expirationDate = "12/25",
+            city = "Istanbul",
+            district = "Kadikoy",
+            fullAddress = "123 Example Street",
+            productId = 1,
+            imageOne = "",
+            title = "Smartphone",
+            quantity = 2,
+            price = 999.99
+        )
+        productRepositoryImpl.addPaymentDetails(payment)
+        verify(mockProductDao).addPaymentDetails(payment)
+    }
+
+    @Test
+    fun `test getUserOrders`() = runBlocking {
+        val expectedOrder = listOf(
+            PaymentEntity(
+                paymentId = 12,
+                userId = "123",
+                cardNumber = "4111111111111111",
+                cardHolderName = "John Doe",
+                expirationDate = "12/25",
+                city = "Istanbul",
+                district = "Kadikoy",
+                fullAddress = "123 Example Street",
+                productId = 1,
+                imageOne = "",
+                title = "Smartphone",
+                quantity = 2,
+                price = 999.99
+            )
+        )
+        `when`(mockProductDao.getUserOrders("123")).thenReturn(flowOf(expectedOrder))
+        val resultFlow = productRepositoryImpl.getUserOrders("123")
+        resultFlow.collect() { result ->
+            assertEquals(expectedOrder, result)
+        }
+    }
+
+    @Test
+    fun `test processOrder`() = runBlocking {
+        val userId = "123"
+        val expectedPayment = PaymentEntity(
+            paymentId = 12,
+            userId = "123",
+            cardNumber = "4111111111111111",
+            cardHolderName = "John Doe",
+            expirationDate = "12/25",
+            city = "Istanbul",
+            district = "Kadikoy",
+            fullAddress = "123 Example Street",
+            productId = 1,
+            imageOne = "",
+            title = "Smartphone",
+            quantity = 2,
+            price = 999.99
+        )
+        productRepositoryImpl.processOrder(userId, expectedPayment)
+        verify(mockProductDao).processOrder(userId, expectedPayment)
+    }
+
 }
